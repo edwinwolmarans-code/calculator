b1f69b1fc5818bd00d0d6d8ae94a4aaa53998a5b
Edwin <142680250+edwinwolmarans-code@users.noreply.github.com>
2024-08-25 21:03:22 +0200

    feat: add mathematical operators

---
+++ script.js
@@ -0,0 +1,23 @@
+let x;
+let y;
+
+// mathematical operators
+function add(x, y) {
+  let a = x + y;
+  return a;
+}
+
+function substract(x, y) {
+  let a = x - y;
+  return a;
+}
+
+function multiply(x, y) {
+  let a = x * y;
+  return a;
+}
+
+function divide(x, y) {
+  let a = x / y;
+  return a;
+}
